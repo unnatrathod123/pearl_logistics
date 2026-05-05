bec2351e8e89112a74f3ea5de62e8b1b215146f5
Maan0882 <gajjarmansi2808@gmail.com>
2026-05-05 14:54:13 +0530

    feat: changes image in home page hero section

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         {/* Background image */}
         <div className="absolute inset-0 w-full h-full animate-fade-in">
           <Image
-            src="/wheat.jpg"
+            src="/grain_in_hand.jpg"
             alt="Golden wheat field representing our core agricultural commodities"
             fill
             className="object-cover brightness-[0.5] hover:scale-105 transition-transform duration-10000"
